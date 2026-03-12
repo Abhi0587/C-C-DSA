b8e0b6028359849d8e0b858e7cbe7f6f96dfd0c1
Abhilash U S <163625900+Abhi0587@users.noreply.github.com>
2026-03-12 12:10:26 +0530

    Time: 0 ms (100%), Space: 11.5 MB (9.15%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
@@ -0,0 +1,44 @@
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+
+    struct ListNode* third=NULL;
+    struct ListNode* last=NULL;
+
+    if(list1==NULL)return list2;
+    if(list2==NULL)return list1;
+
+    if(list1->val < list2->val)
+    {
+        third=last=list1;
+        list1=list1->next;
+        last->next=NULL;
+    }
+    else
+    {
+        third=last=list2;
+        list2=list2->next;
+        last->next=NULL;
+    }
+    while(list1!=NULL && list2!=NULL)
+    {
+        if(list1->val < list2->val)
+        {
+            last->next=list1;
+            last=list1;
+            list1=list1->next;
+            last->next=NULL;
+        }
+        else
+        {
+            last->next=list2;
+            last=list2;
+            list2=list2->next;
+            last->next=NULL;
+        }
+    }
+    if(list1!=NULL)
+      last->next=list1; 
+    else
+        last->next=list2;
+    return third;
+}
