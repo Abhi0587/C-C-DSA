1b23a21a963a1468c8e71023f7277efac12ec752
Abhilash U S <163625900+Abhi0587@users.noreply.github.com>
2026-07-03 21:20:05 +0530

    Time: 0 ms (100%), Space: 9.2 MB (48.44%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,18 @@
+#include<limits.h>
+
+int reverse(int x){
+    long long rev=0;
+    int rem=0;
+    while(x!=0){
+        rem=x%10;
+        rev=rev*10+rem;
+        x=x/10;
+
+    }
+
+    if(rev>INT_MAX || rev<INT_MIN)
+    return 0;
+
+ return rev;
+
+}
