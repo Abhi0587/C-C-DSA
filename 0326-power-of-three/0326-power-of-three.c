8423a503c86b95a7071eb361875c8e665014a1c3
Abhilash U S <163625900+Abhi0587@users.noreply.github.com>
2026-07-11 23:38:02 +0530

    Time: 3 ms (57.12%), Space: 9.2 MB (77.26%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.c
+++ 0326-power-of-three/0326-power-of-three.c
@@ -1,12 +1,13 @@
 bool isPowerOfThree(int n) {
-    if(n==0)
+    if(n<=0)
     return false;
 
-    if(n==1)
-    return true;
-
-    while(n%3==0){
+    while(n>1){
+        if(n%3!=0){
+            return false;
+        }
         n/=3;
     }
-    return n==1;
+    return true;
+   
 }
